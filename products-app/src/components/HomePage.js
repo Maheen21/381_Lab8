48b6b844462bc1e525b919e9cd6db9bbae19b376
Mehak <kaurmehak1007@gmail.com>
2024-03-13 14:53:59 -0600

    names change

--- products-app/src/components/HomePage.js
+++ products-app/src/components/HomePage.js
@@ -38,11 +38,11 @@ const HomePage = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td style={styles.td}>A B</td>
+                        <td style={styles.td}>Maheen Aftab</td>
                         <td style={styles.td}>Developer</td>
                     </tr>
                     <tr>
-                        <td style={styles.td}>C D</td>
+                        <td style={styles.td}>Mehakdeep Kaur</td>
                         <td style={styles.td}>Designer</td>
                     </tr>
                 </tbody>
